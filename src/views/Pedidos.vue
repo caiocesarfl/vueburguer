637588b6c65181b711a404e3201cca2dfaa059f6
Alexandre Iabrudi <alexandre.iabrudi@dtidigital.com.br>
2023-09-21 23:35:46 -0300

    first commit

---
+++ src/views/Pedidos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="main-container">
+      <h1>Gerenciar pedidos:</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Pedidos',
+  components: {
+  }
+}
+</script>
